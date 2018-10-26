7457f98cd200f7e1adf51788bcde82e222e437d4
evapodaca87 <evapodaca87@gmail.com>
2018-10-26 11:23:49 -0600

    added router for all one, and put post and delete

--- routes/cakes.js
+++ routes/cakes.js
@@ -1,57 +1,54 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-
-let cakes = require('../cakes.json')
+let cakes = require('../cakes.json');
 
 router.get('/', (req, res) => {
-	res.json({ cakes: cakes })
-})
+    res.json({ cakes: cakes });
+});
 
 router.get('/:id', (req, res, next) => {
-	const id = req.params.id
-	console.log(req.params.id)
-})
+    const id = req.params.id;
+    console.log(id);
 
-const cake = cakes.filter(cake => {
-	return cake.id == id
-})
-
-if (!cake.length) {
-	next()
-}
-
-res.json({ cake: cake[0] })
+    const cake = cakes.filter((cake) => {
+        return cake.id == id;
+    });
+    if (!cake.length) {
+        next();
+    }
+    res.json({ cake: cake[0] });
+});
 
 router.post('/', (req, res, next) => {
-	const body = req.body
-	console.log(bopdy)
-	cakes.push(body)
-	res.json({ cakes: cakes })
-})
+    const body = req.body;
+    console.log(bopdy);
+    cakes.push(body);
+    res.json({ cakes: cakes });
+});
 
 router.put('/:id', (req, res) => {
-	const id = req.params.id
-	const body = req.body
+    const id = req.params.id;
+    const body = req.body;
 
-	console.log(id)
-	console.log(body)
+    console.log(id);
+    console.log(body);
 
-	const updatedCakes = cakes.map(cake => {
-		if (cake.id == id) {
-			return body
-		}
-		return cake
-	})
-	res.json({ cakes: updatedCakes })
-})
+    const updatedCakes = cakes.map((cake) => {
+        if (cake.id == id) {
+            return body;
+        }
+        return cake;
+    });
+    res.json({ cakes: updatedCakes });
+});
 
 router.delete('/:id', (req, res) => {
-	const id = req.params.id
-	const trash = cakes.filter(cake => {
-		return cake.id != id
-	})
-	res.json({ cakes: trash })
-})
+    const id = req.params.id;
+    const trash = cakes.filter((cake) => {
+        return cake.id != id;
+    });
+    res.json({ cakes: trash });
+});
 
-module.exports = router
+module.exports = router;
