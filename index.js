d8d0378dd3841ef892d943dbc91cb9ebb98d2ace
evapodaca87 <evapodaca87@gmail.com>
2018-10-26 10:37:06 -0600

    first round

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 let deltron = 3030;
+const cors = require('cors');
+
+const cakeRoutes = require('./routes/cakes');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 app.get('/', (req, res) => res.send('🐧🐧🐧🐧🐧🐧🐧'));
 
-app.listen(deltron, () => console.log('server running on port 3030'));
+// app.use('/cakes', cakeRoutes);
+
+app.listen(deltron, () => console.log(`server running on port ${deltron}`));
